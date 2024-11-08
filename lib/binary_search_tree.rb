4eadae830aed4920cdcc4c369972de4fe3a7e898
zondiMaqina <158191395+zondiMaqina@users.noreply.github.com>
2024-11-08 07:59:38 +0000

    add method for creating binary search tree

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -8,3 +8,23 @@ class Node
     @right = right
   end
 end
+
+class Tree
+  def initialize(array)
+    @root = build_tree(array, 0, array.size - 1)
+  end
+
+  def build_tree(array, start, endpoint)
+    return nil if array.empty?
+    return nil if start > endpoint
+    sorted_array = MergeSort.new.merge_sort(array).uniq # returns sorted array with no duplicates
+    mid = (start + endpoint) / 2
+    root = Node.new(array[mid]) # creates new node
+    root.left = build_tree(sorted_array, start, mid - 1)
+    root.right = build_tree(sorted_array, mid + 1, endpoint)
+    return root
+  end
+end
+array = [1, 2, 3, 4]
+tree = Tree.new(array)
+p tree
