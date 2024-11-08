dbe0473a3542eb7ce556f189c93355722ee13e5b
zondiMaqina <158191395+zondiMaqina@users.noreply.github.com>
2024-11-08 07:46:26 +0000

    initialize node class

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,4 +1,10 @@
 require_relative 'merge_sort'
-array = [1, 3, 5, 7, 9, 2, 4, 6, 8, 0]
-sort = MergeSort.new
-puts sort.merge_sort(array)
+
+class Node
+  attr_accessor :value, :left, :right
+  def initialize(value, left = nil, right = nil)
+    @value = value
+    @left = left
+    @right = right
+  end
+end
